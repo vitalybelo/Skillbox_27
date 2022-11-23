8856774f94c6ac944b102619d40721be897a83ff
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-23 17:24:37 +0300

    init

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,52 @@
 #include "Branch.h"
 using namespace std;
 
+Branch* generateTree (const string &name);
 
 int main() {
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> bigBranchDist(BIG_BRANCH_MIN,BIG_BRANCH_MAX);
-    std::uniform_int_distribution<> midBranchDist(MIDDLE_BRANCH_MIN,MIDDLE_BRANCH_MAX);
 
-    // корневая ветка
-    Branch root("ROOT", nullptr);
-
-    Branch tree("TREE",&root);
-    root.addBranch(&tree);
-
-    tree.addBranch(new Branch("BIG_1",&tree));
-    tree.addBranch(new Branch("BIG_2",&tree));
-    tree.addBranch(new Branch("BIG_3",&tree));
+    Branch* tree = generateTree("T1_");
 
 
-    cout << "ROOT = " << root.getBranch().size() << " :: " << &root << endl;
-    cout << "TREE = " << tree.getBranch().size() << " :: " << tree.getParent() << " :: ";
-    cout << tree.getBranch().at(0)->getHouseKeeper() << ", ";
-    cout << tree.getBranch().at(1)->getHouseKeeper() << ", ";
-    cout << tree.getBranch().at(2)->getHouseKeeper() << endl;
+    cout << "TREE: " << tree->size() << endl;
+    cout << "BIG BRANCH: " << tree->getBranch().at(0)->getBranch().size() << endl;
 
-    Branch* b = root.getBranch().at(0);
-    cout << "TREE = " << b->getBranch().size() << " :: " << b->getParent() << " :: ";
-    cout << b->getBranch().at(0)->getHouseKeeper() << ", ";
-    cout << b->getBranch().at(1)->getHouseKeeper() << ", ";
-    cout << b->getBranch().at(2)->getHouseKeeper() << endl;
+    Branch* t = tree->getBranch().at(0);
+    cout << "\tNames: ";
+    for (int i = 0; i < t->size(); i++) {
+        Branch* b1 = t->getBranch().at(i);
+        cout << b1->getHouseKeeper() << ", ";
+
+
+    }
+    cout << endl;
 
     return 0;
+}
+
+Branch* generateTree (const string &name) {
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> bigDist(BIG_BRANCH_MIN, BIG_BRANCH_MAX);
+    std::uniform_int_distribution<> middleDist(MIDDLE_BRANCH_MIN, MIDDLE_BRANCH_MAX);
+
+    auto* tree = new Branch("TREE", nullptr);           // корневой узел (он же ствол)
+    auto* branch1 = new Branch("BIG BRANCHES", tree);   // узел "большие ветки" (или "Бельдяшки")
+    tree->addBranch(branch1);                                   // единственная ветка корневого узла
+
+    for (int i = 0; i < bigDist(gen); i++) {
+        // добавляем большие ветки
+        string name1 = name + "Boss_" + to_string(i + 1);
+        branch1->addBranch(new Branch(name1, tree));
+        // добавляем средние ветки
+        for(int j = 0; j < middleDist(gen); )
+
+    }
+
+    //Branch middleBranch("MIDDLE BRANCHES", &branch1); // средние ветки
+
+
+    return tree;
 }
